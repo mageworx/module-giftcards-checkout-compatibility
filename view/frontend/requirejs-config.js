ddc1606f96de13acfd854f544f2c033e4314ff20
Siarhey Uchukhlebau <siarhey.uchukhlebau@gmail.com>
2020-09-21 16:34:16 +0300

    Initial Commit

---
+++ view/frontend/requirejs-config.js
@@ -0,0 +1,9 @@
+var config = {
+    config: {
+        mixins: {
+            'MageWorx_GiftCards/js/view/payment/mageworx-giftcards': {
+                'MageWorx_GiftCardsCheckout/js/view/summary/additional-inputs/mageworx-gift-cards-form-mixin': true
+            }
+        }
+    }
+};
